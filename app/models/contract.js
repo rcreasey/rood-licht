e9a6b10a1c684048ee575969dfb700b271b307c3
Ryan C. Creasey <rcreasey@blizzard.com>
2013-09-11 22:30:40 -0700

    Cleaning up contract model.

--- app/models/contract.js
+++ app/models/contract.js
@@ -9,8 +9,10 @@ var Contract = Schema({
   issuerCorpName: {type: String, default: null},
   startStationID: Number,
   startStationName: {type: String, default: null},
+  startSolarSystemID: Number,
   endStationID: Number,
   endStationName: {type: String, default: null},
+  endSolarSystemID: Number,
   type: String,
   status: String,
   title: String,
@@ -26,39 +28,5 @@ var Contract = Schema({
   volume: Number
 });
 
-var station_id_to_name = function(stationID) {
-  switch(stationID) {
-    case 60003760:
-      return 'Jita 4-4 - CNAP';
-    case 61000299:
-      return 'MZ1E-P IX';
-    case 61000005:
-      return 'JU-OWQ VII';
-    case 60014945:
-      return '1DH-SX III';
-    case 61000854:
-      return '4-EP12 VIII';
-    case 60012334:
-      return 'Jan VI - Moon 21';
-    case 60014917:
-      return 'VFK-IV VI';
-    case 61000358:
-      return '85-B52 IV';
-    case 61000827:
-      return 'E-FIC0 I';
-    case 61000166:
-      return 'S-DN5M IX';
-    default:
-      return stationID;
-  }
-};
-
-Contract.methods.startStation = function () {
-  return station_id_to_name( this.startStationID );
-};
-
-Contract.methods.endStation = function () {
-  return station_id_to_name( this.endStationID );
-};
 
 module.exports = mongoose.model('Contract', Contract);
