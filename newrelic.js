12afecc7db6b7049577364c69ab1e03bf5973a6c
Ryan C. Creasey <rcreasey@blizzard.com>
2013-11-03 16:51:01 -0800

    re-enabling newrelic.

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,8 @@
 exports.config = {
   app_name : ['Rood Licht Handel BV'],
   license_key : process.env.NEW_RELIC_LICENSE_KEY,
+  agent_enabled : 'true',
   logging : {
-    level : 'info'
+    level : 'debug'
   }
 };
