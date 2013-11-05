46ec8e89e8ddba3b9ffa13a98d61d2c1bf5e9899
Ryan C. Creasey <rcreasey@blizzard.com>
2013-11-05 00:51:07 -0800

    settings admin done.

--- app/controllers/settings_controller.js
+++ app/controllers/settings_controller.js
@@ -1,17 +1,30 @@
 var locomotive = require('locomotive')
+  , async = require('async')
+  , _ = require('underscore')
   , Controller = locomotive.Controller
   , SettingsController = new Controller()
   , Settings = require('../models/setting')
 
 SettingsController.index = function(req, res) {
-  Settings.find({}).sort('key').exec(function(err, settings) {
+  async.waterfall([
+    function(callback) {
+      Settings.find({}).sort('key').exec( callback );
+    }, function(results, callback) {
+      Settings.initialize(results, callback)
+    }
+  ], function(settings) {
     res.render('settings/index', {title: 'Settings', user: req.user, settings: settings});
-  })
+  });
+
 };
 
 SettingsController.update = function(req, res) {
-  console.log(req.body)
-  res.redirect('/settings');
+  async.forEach(Object.keys(req.body.keys), function(key, callback) {
+    Settings.update({key: key}, {$set: {key: key, value: req.body.keys[key]}}, {upsert: true}, callback);
+  }, function(err) {
+    res.redirect('/settings');
+  });
+
 };
 
 module.exports = SettingsController;
--- app/models/setting.js
+++ app/models/setting.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
+  , _ = require('underscore')
 
 var Setting = Schema({
   key: {type: String, index: true, unique: true},
   value: String
 });
+
+Setting.statics.initialize = function (results, callback) {
+  settings = {};
+  _.map(results, function(setting, value){ return settings[ setting.key ] = setting.value; });
+  callback( settings );
+}
 
 module.exports = mongoose.model('Setting', Setting);
